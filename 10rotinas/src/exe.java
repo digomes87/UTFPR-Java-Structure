9396467ff4806c8b0636833dbbb460eb3d48fdc8
Diego G <diego.gomes87@gmail.com>
2020-03-04 19:15:35 -0300

    string

---
+++ 10rotinas/src/exe.java
@@ -0,0 +1,10 @@
+
+public class exe {
+   public static void main(String args[]){
+       String java = "Java";
+       String standart = "SE";
+
+       String jdk = java.concat(" ").concat(standart);
+       System.out.println(jdk);
+   }
+}
